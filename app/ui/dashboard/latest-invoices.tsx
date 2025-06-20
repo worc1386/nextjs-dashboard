a98b8195b9f11a4ad872aebde445dcbb235d51f2
worc1386 <worc1386@gmail.com>
2025-06-20 10:28:55 +0100

    jkjkjkjk

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,11 +1,4 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
-import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
-import { LatestInvoice } from '@/app/lib/definitions';
-import { fetchLatestInvoices } from '@/app/lib/data';
-
-export default async function LatestInvoices(): Promise<JSX.Element> {
+export default async function LatestInvoices() {
   const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
 
   return (
