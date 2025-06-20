4bcbb0b219dee2963598870035938c52d4e76f6d
worc1386 <worc1386@gmail.com>
2025-06-20 10:34:11 +0100

    jkjk

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,11 +1,20 @@
+// app/ui/dashboard/latest-invoices.tsx
+
+import { ArrowPathIcon } from '@heroicons/react/24/outline';
+import clsx from 'clsx';
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchLatestInvoices } from '@/app/lib/data';
+
 export default async function LatestInvoices() {
-  const latestInvoices: LatestInvoice[] = await fetchLatestInvoices();
+  const latestInvoices = await fetchLatestInvoices();
 
   return (
     <div className="flex w-full flex-col md:col-span-4">
       <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Latest Invoices
       </h2>
+
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
         <div className="bg-white px-6">
           {latestInvoices.map((invoice, i) => (
