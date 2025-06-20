dd9c651d1c21ca0d62ed44ec72048d0913df9e52
worc1386 <worc1386@gmail.com>
2025-06-20 09:23:59 +0100

    hjhj

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,3 +1,6 @@
-export default function Page() {
+// app/dashboard/invoices/page.tsx
+export default async function Page() {
+  // simulate slow data fetch
+  await new Promise((r) => setTimeout(r, 500));
   return <p>Invoices Page</p>;
 }
